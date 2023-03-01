643143fa29c966edd0ee665f39bfe43d68571b07
andree262 <andreemedeiro@gmail.com>
2023-03-01 17:47:17 -0300

    criado modulo material

--- ANGULAR/vendas/src/app/products/products.module.ts
+++ ANGULAR/vendas/src/app/products/products.module.ts
@@ -1,24 +1,13 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
 
-import { ProductsRoutingModule } from './products-routing.module';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatDialogModule } from '@angular/material/dialog';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatTableModule } from '@angular/material/table';
-import { MatIconModule } from '@angular/material/icon';
-import { ProductSearchComponent } from './product-search/product-search.component';
-import { MatRadioModule } from '@angular/material/radio';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatPaginatorModule} from '@angular/material/paginator';
-import {MatSortModule} from '@angular/material/sort';
-import { ProductListComponent } from './product-list/product-list.component';
-import { ProductCreateComponent } from './product-create/product-create.component';
+import { MaterialModule } from '../shared/material/material.module';
 import { ProductsComponent } from './containers/product/products.component';
+import { ProductCreateComponent } from './product-create/product-create.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductSearchComponent } from './product-search/product-search.component';
+import { ProductsRoutingModule } from './products-routing.module';
 
 @NgModule({
   declarations: [
@@ -28,23 +17,11 @@ import { ProductsComponent } from './containers/product/products.component';
     ProductSearchComponent,
   ],
   imports: [
+    MaterialModule,
     CommonModule,
     ProductsRoutingModule,
-    MatCardModule,
-    MatButtonModule,
-    MatDialogModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatIconModule,
-    MatRadioModule,
     FormsModule,
-    MatProgressSpinnerModule,
-    MatPaginatorModule,
-    MatSortModule
-
   ],
   exports: [
     ProductListComponent,
---
+++ ANGULAR/vendas/src/app/shared/material/material.module.ts
@@ -0,0 +1,38 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],exports:[
+    MatButtonModule,
+    MatCardModule,
+    MatTableModule,
+    MatSortModule,
+    MatSnackBarModule,
+    MatRadioModule,
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatInputModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatDialogModule
+  ]
+})
+export class MaterialModule { }
